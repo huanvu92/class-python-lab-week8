068cea81df877b9aad416ceb993a0a3570cba06a
huanvu92 <burberryvu@gmail.com>
2023-03-21 18:09:56 -0700

    Create functions-lab1Example.py

---
+++ functions-lab1Example.py
@@ -0,0 +1,21 @@
+"""
+functions lab example code
+"""
+
+
+def foo(num):
+    """
+     f(num) = 3*num + 10
+    """
+    # students should write code here
+
+
+def bar(first, second, third):
+    """
+     Create a function bar that takes 3 input arguments (first, second, and third)
+     and returns a list containing the input values in a sorted list.
+    """
+    # students should write code here
+
+
+# students should write code to run foo and bar here
